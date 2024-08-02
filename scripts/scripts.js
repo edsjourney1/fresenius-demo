75a4a8fbd860a422693b5dc8b9aa5d2de309cebe
Upadhyaya <305846@cognizant.com>
2024-08-02 20:59:56 +0530

    hero block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,15 +18,37 @@ import {
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
+// function buildHeroBlock(main) {
+//   const h1 = main.querySelector('h1');
+//   const picture = main.querySelector('picture');
+//   // eslint-disable-next-line no-bitwise
+//   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+//     const section = document.createElement('div');
+//     section.append(buildBlock('hero', { elems: [picture, h1] }));
+//     main.prepend(section);
+//   }
+// }
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
+    const h1 = main.querySelector('h1');
+    const pTag = main.querySelector('p');
+    const picture = main.querySelector('picture');
+
+    // eslint-disable-next-line no-bitwise
+    if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+        const section = document.createElement('div');
+        const heroContent = document.createElement('div'); // Create a new div with class 'heroContent'
+        heroContent.classList.add('heroContent'); // Add the class 'heroContent' to the div
+
+        // Append the h1 element to the heroContent div
+        heroContent.append(h1);
+        heroContent.append(pTag);
+
+        // Append the heroContent div and picture element to the section
+        section.append(buildBlock('hero', { elems: [picture, heroContent] }));
+
+        // Prepend the section to the main content area
+        main.prepend(section);
+    }
 }
 
 /**
